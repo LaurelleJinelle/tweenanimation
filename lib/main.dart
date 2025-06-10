a169d909379ec7795c5ddcdd0c08b13662b05cfc
mangaorphy <o.manga@alustudent.com>
2025-06-10 21:48:25 +0200

    image showing the tween animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tween Animation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+        home: TweenAnimationScreen(),
+      debugShowCheckedModeBanner: false,
+      );
+    }
+  }
+
 class TweenAnimationScreen extends StatelessWidget {
+  const TweenAnimationScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +34,7 @@ class TweenAnimationScreen extends StatelessWidget {
 
 class TweenAnimationBody extends StatefulWidget {
   @override
+  // ignore: library_private_types_in_public_api
   _TweenAnimationBodyState createState() => _TweenAnimationBodyState();
 }
 
